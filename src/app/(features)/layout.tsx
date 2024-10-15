2b369dcb133c9c9d9bc4b54ccc9cd1e00d8519c0
mohamadazaruddin <azaruddin1307@gmail.com>
2024-10-16 01:49:50 +0530

    whatsapp chat added

--- src/app/(features)/layout.tsx
+++ src/app/(features)/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import WhatsAppChat from "../components/WhatsappChat";
 
 export default function layout({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <Header name={`Amcen homes`} />
+      <WhatsAppChat isOpen={isOpen} setIsOpen={setIsOpen} />
       {children}
       <Footer />
     </div>
---
+++ src/app/components/WhatsappChat.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Text,
+  VStack,
+  Collapse,
+  Avatar,
+} from "@chakra-ui/react";
+import WhatsappOutlineIcon from "./Icons/WhatsappOutlineIcon";
+
+const WhatsAppChat = ({ isOpen, setIsOpen }: any) => {
+  const togglePanel = () => {
+    setIsOpen(!isOpen);
+  };
+  const phoneNumber = "9702914246"; // e.g., '1234567890'
+  const message = "Hello, I have a question about my project."; // Default message
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+  return (
+    <Box position="fixed" bottom="40px" zIndex={9999} right="40px">
+      <Box position="relative">
+        <IconButton
+          aria-label="WhatsApp Chat"
+          icon={<WhatsappOutlineIcon h="35px" w="35px" />}
+          h="50px"
+          w="50px"
+          colorScheme="green"
+          onClick={togglePanel}
+          borderRadius="full"
+        />
+
+        <Box
+          bg="white"
+          borderRadius="md"
+          boxShadow="lg"
+          transition="0.5s linear"
+          display={isOpen ? "block" : "none"}
+          mt={2}
+          bottom="45px"
+          right="45px"
+          width="250px"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          backgroundSize="cover"
+          backgroundImage="url('/images/whatsappbg.png')"
+          position="absolute"
+        >
+          <Flex
+            bg="#0A5F54"
+            px="4"
+            py="2"
+            borderTopLeftRadius="md"
+            borderTopRightRadius="md"
+          >
+            <Avatar
+              src="/images/appLogo.png"
+              name="Amcen Homes"
+              bg="#fff"
+              size="sm"
+            />
+            <Text fontSize="md" fontWeight="medium" color="#ffffff">
+              Amcen Homes
+            </Text>
+          </Flex>
+
+          <VStack align="start" spacing={3} p="4">
+            <Box
+              bg="#ffffff"
+              p={2}
+              borderRadius="md"
+              width="fit-content"
+              mb="20"
+            >
+              <Text fontWeight="medium" fontSize="sm" color="#494848">
+                Amcen Homes:
+              </Text>
+              <Text fontSize="sm" fontWeight="semibold" mt="1">
+                Hi there! <br /> How can I help you today?
+              </Text>
+            </Box>
+          </VStack>
+          <Button
+            bg="#0A5F54"
+            color="#fff"
+            _hover={{
+              bg: "#0A5F54",
+              color: "#fff",
+            }}
+            width="100%"
+            rounded="md"
+            borderTopEndRadius="0"
+            borderTopLeftRadius="0"
+            mt={3}
+            onClick={() => {
+              window.open(whatsappLink, "_blank");
+            }}
+          >
+            Start Chat
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default WhatsAppChat;
